a03a40346b3bd6cbe11928843cf1a7c08641c5ba
Abdus Sobhan <sobhangtavc@gmail.com>
2026-07-04 21:07:07 +0600

    Auto update

---
+++ CodeWithHarry - Python Tutorial For Beginners in Hindi/Chapter 4 - Lists and Tuples/2.py
@@ -0,0 +1,17 @@
+marks = []
+
+f1 = int(input("Enter first mark: "))
+marks.append(f1)
+f2 = int(input("Enter second mark: "))
+marks.append(f2)
+f3 = int(input("Enter third mark: "))
+marks.append(f3)
+f4 = int(input("Enter fourth mark: "))
+marks.append(f4)
+f5 = int(input("Enter fifth mark: "))
+marks.append(f5)
+f6 = int(input("Enter sixth mark: "))
+marks.append(f6)
+
+marks.sort()
+print(marks)
